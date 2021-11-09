c8b1db6542c47f31159328e57219cc3709c4aa6e
yassine-youcefi <mohamed.youcefi.etu@univ-mosta.dz>
2021-11-09 13:43:12 +0100

    update data-prep

--- data_prep.py
+++ data_prep.py
@@ -5,91 +5,27 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 class DataPrep():
     def __init__(self):
         self.trait_cat_dict = {
-            'O': 'cOPN',
-            'C': 'cCON',
-            'E': 'cEXT',
-            'A': 'cAGR',
-            'N': 'cNEU',
             'OPN': 'cOPN',
             'CON': 'cCON',
             'EXT': 'cEXT',
             'AGR': 'cAGR',
             'NEU': 'cNEU',
-            'Openness': 'cOPN',
-            'Conscientiousness': 'cCON',
-            'Extraversion': 'cEXT',
-            'Agreeableness': 'cAGR',
-            'Neuroticism': 'cNEU'
         }
         self.trait_score_dict = {
-            'O': 'sOPN',
-            'C': 'sCON',
-            'E': 'sEXT',
-            'A': 'sAGR',
-            'N': 'sNEU',
             'OPN': 'sOPN',
             'CON': 'sCON',
             'EXT': 'sEXT',
             'AGR': 'sAGR',
             'NEU': 'sNEU',
-            'Openness': 'sOPN',
-            'Conscientiousness': 'sCON',
-            'Extraversion': 'sEXT',
-            'Agreeableness': 'sAGR',
-            'Neuroticism': 'sNEU'
         }
-        self.LIWC_features = [
-            'WPS', 'Unique', 'Dic', 'Sixltr', 'Negate', 'Assent', 'Article', 'Preps', 'Number',
-            'Pronoun', 'I', 'We', 'Self', 'You', 'Other',
-            'Affect', 'Posemo', 'Posfeel', 'Optim', 'Negemo', 'Anx', 'Anger', 'Sad',
-            'Cogmech', 'Cause', 'Insight', 'Discrep', 'Inhib', 'Tentat', 'Certain',
-            'Senses', 'See', 'Hear', 'Feel',
-            'Social', 'Comm', 'Othref', 'Friends', 'Family', 'Humans',
-            'Time', 'Past', 'Present', 'Future',
-            'Space', 'Up', 'Down', 'Incl', 'Excl', 'Motion',
-            'Occup', 'School', 'Job', 'Achieve',
-            'Leisure', 'Home', 'Sports', 'TV', 'Music',
-            'Money',
-            'Metaph', 'Relig', 'Death', 'Physcal', 'Body', 'Sexual', 'Eating', 'Sleep', 'Groom',
-            'Allpct', 'Period', 'Comma', 'Colon', 'Semic', 'Qmark', 'Exclam', 'Dash', 'Quote', 'Apostro', 'Parenth', 'Otherp',
-            'Swear', 'Nonfl', 'Fillers',
-        ]
 
     def prep_data(self, type, trait, regression=False, model_comparison=False):
         df_status = self.prep_status_data()
 
         tfidf = TfidfVectorizer(stop_words='english', strip_accents='ascii')
 
-        if type == 'essay':
-
-            if model_comparison:
-                X = tfidf.fit_transform(df_essay['TEXT'])
-            # Data for fitting production model
-            else:
-                X = df_essay['TEXT']
-
-            y_column = self.trait_cat_dict[trait]
-            y = df_essay[y_column]
-
-        elif type == 'status':
-            # Include other features with tfidf vector
-            other_features_columns = [
-                'NETWORKSIZE',
-                'BETWEENNESS',
-                'NBETWEENNESS',
-                'DENSITY',
-                'BROKERAGE',
-                'NBROKERAGE',
-                'TRANSITIVITY'
-            ]
-
-            # If need data to compare models
-            if model_comparison:
-                X = tfidf.fit_transform(df_status['STATUS'])
-                print('X ')
-            # Data to fit production model
-            else:
-                X = df_status['STATUS']
+        if type == 'status':        
+            X = df_status['STATUS']
             
             if regression:
                 y_column = self.trait_score_dict[trait]
@@ -114,6 +50,3 @@ class DataPrep():
             df[trait] = df[trait].map(d)
 
         return df
-
-    def load_data(self, filepath):
-        return pd.read_csv(filepath, encoding="ISO-8859-1")
